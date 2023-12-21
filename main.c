1e5cc7ce5646114e50d78ce6c6b1647d99bb1ab8
Abdulgaffar Gülice <abdulgaffar.gulice@gazi.edu.tr>
2023-12-21 16:45:29 +0300

    ödev
    abdulgaffar gülice23181616048 ,yunus kavaklı23181617005,cem civelek23181617001

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void matris_top(int *adr1,int *adr2){
+    printf("toplam dizisi = ");
+    int toplam[2][3];
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            printf("%d,",toplam [i][j]= *(adr1+i+j)+*(adr2+i+j));
+
+        }
+
+    }
+}
+int main() {
+int matris1 [2][3] = {1,2,3,4,5,7};
+int matris2 [2][3] = {5,6,7,8,9,10} ;
+    matris_top(&matris1,&matris2);
+    return 0;
+}
